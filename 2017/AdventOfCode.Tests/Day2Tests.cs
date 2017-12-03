8429f32e4ac4b61985a7a4e974dacc6a18fc9a1b
Dave van Herten <dvanherten@outlook.com>
2017-12-02 20:48:22 -0500

    Re-org Day2

--- 2017/AdventOfCode.Tests/Day2Tests.cs
+++ 2017/AdventOfCode.Tests/Day2Tests.cs
@@ -7,33 +7,53 @@ namespace AdventOfCode.Tests
 {
     public class Day2Tests
     {
-        [Fact]
-        public void Part1Tests()
+        [Theory]
+        [InlineData("5 1 9 5", 8)]
+        [InlineData("7 5 3", 4)]
+        [InlineData("2 4 6 8", 6)]
+        public void Part1LineTests(string input, int expected)
         {
-            var input = new[] { "5 1 9 5", "7 5 3", "2 4 6 8" };
-            var result = Day2.SolvePart1(input);
-            Assert.Equal(18, result);
+            var result = Day2.Part1LineLogic(input);
+            Assert.Equal(expected, result);
         }
 
         [Fact]
-        public void Part2Tests()
+        public void Part1FullTest()
         {
-            var input = new[] { "5 9 2 8", "9 4 7 3", "3 8 6 5" };
-            var result = Day2.SolvePart2(input);
-            Assert.Equal(9, result);
+            var input = new[] { "5 1 9 5", "7 5 3", "2 4 6 8" };
+            var result = Day2.Solve(input, Day2.Part1LineLogic);
+            Assert.Equal(18, result);
         }
 
         [Fact]
         public void Part1RealPuzzle()
         {
-            var result = Day2.SolvePart1(GetPuzzleInput());
+            var result = Day2.Solve(GetPuzzleInput(), Day2.Part1LineLogic);
             Assert.Equal(47136, result);
+        }
+
+        [Theory]
+        [InlineData("5 9 2 8", 4)]
+        [InlineData("9 4 7 3", 3)]
+        [InlineData("3 8 6 5", 2)]
+        public void Part2LineTests(string input, int expected)
+        {
+            var result = Day2.Part2LineLogic(input);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2FullTest()
+        {
+            var input = new[] { "5 9 2 8", "9 4 7 3", "3 8 6 5" };
+            var result = Day2.Solve(input, Day2.Part2LineLogic);
+            Assert.Equal(9, result);
         }
 
         [Fact]
         public void Part2RealPuzzle()
         {
-            var result = Day2.SolvePart2(GetPuzzleInput());
+            var result = Day2.Solve(GetPuzzleInput(), Day2.Part2LineLogic);
             Assert.Equal(250, result);
         }
 
--- 2017/AdventOfCode/Day2.cs
+++ 2017/AdventOfCode/Day2.cs
@@ -5,36 +5,35 @@ namespace AdventOfCode
 {
     public static class Day2
     {
-        public static int SolvePart1(string[] input)
+        public static int Solve(string[] input, Func<string, int> lineLogic)
         {
-            return input.Sum(x =>
-            {
-                var split = x.Split(null).Select(int.Parse).ToArray();
-                return split.Max() - split.Min();
-            });
+            return input.Sum(lineLogic);
         }
 
-        public static int SolvePart2(string[] input)
+        public static Func<string, int> Part1LineLogic = x =>
         {
-            return input.Sum(x =>
-            {
-                var ordered = x.Split(null)
-                    .Select(int.Parse)
-                    .OrderByDescending(y => y)
-                    .ToArray();
+            var split = x.Split(null).Select(int.Parse).ToArray();
+            return split.Max() - split.Min();
+        };
 
-                for (var i = 0; i < ordered.Length; i++)
+        public static Func<string, int> Part2LineLogic = x =>
+        {
+            var ordered = x.Split(null)
+                .Select(int.Parse)
+                .OrderByDescending(y => y)
+                .ToArray();
+
+            for (var i = 0; i < ordered.Length; i++)
+            {
+                for (var j = ordered.Length - 1; j > i; j--)
                 {
-                    for (var j = ordered.Length - 1; j > i; j--)
+                    if (ordered[i] % ordered[j] == 0)
                     {
-                        if (ordered[i] % ordered[j] == 0)
-                        {
-                            return ordered[i] / ordered[j];
-                        }
+                        return ordered[i] / ordered[j];
                     }
                 }
-                throw new Exception("Something went horribly horribly wrong.");
-            });
-        }
+            }
+            throw new Exception("Something went horribly horribly wrong.");
+        };
     }
 }
