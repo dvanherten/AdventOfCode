cec1d052fd644d5726761ac2778da3a76bac9713
Dave van Herten <dvanherten@outlook.com>
2017-12-04 00:21:36 -0500

    Day 4 - Both parts

---
+++ 2017/AdventOfCode.Tests/Day4Tests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using AdventOfCode.Supporting;
+using Xunit;
+
+namespace AdventOfCode.Tests
+{
+    public class Day4Tests
+    {
+        [Fact]
+        public void Part1RealPuzzle()
+        {
+            var result = Day4.SolvePart1(GetPuzzleInput());
+            Assert.Equal(455, result);
+        }
+
+        [Fact]
+        public void Part2RealPuzzle()
+        {
+            var result = Day4.SolvePart2(GetPuzzleInput());
+            Assert.Equal(186, result);
+        }
+
+        private string[] GetPuzzleInput()
+        {
+            return File.ReadAllLines(@"Data\Day4Input.txt");
+        }
+    }
+}
---
+++ 2017/AdventOfCode/Day4.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using AdventOfCode.Supporting;
+using Microsoft.Build.Tasks;
+
+namespace AdventOfCode
+{
+    public class Day4
+    {
+        public static int SolvePart1(string[] input)
+        {
+            var counter = 0;
+            foreach (var line in input)
+            {
+                var split = line.Split(null);
+                if (split.Distinct().Count() == split.Length)
+                    counter++;
+            }
+            return counter;
+        }
+
+        public static int SolvePart2(string[] input)
+        {
+            var counter = 0;
+            foreach (var line in input)
+            {
+                var split = line.Split(null);
+                if (split.Select(x => string.Concat(x.OrderBy(c => c))).Distinct().Count() == split.Length)
+                    counter++;
+            }
+            return counter;
+        }
+    }
+}
