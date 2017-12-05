757e47a062361417aeac0c3af86ca2df861dd28c
Dave van Herten <dvanherten@outlook.com>
2017-12-05 00:12:05 -0500

    Day 5

---
+++ 2017/AdventOfCode.Tests/Day5Tests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using AdventOfCode.Supporting;
+using Xunit;
+
+namespace AdventOfCode.Tests
+{
+    public class Day5Tests
+    {
+        [Fact]
+        public void Part1RealPuzzle()
+        {
+            var result = Day5.SolvePart1(GetPuzzleInput());
+            Assert.Equal(351282, result);
+        }
+
+        [Fact]
+        public void Part2RealPuzzle()
+        {
+            var result = Day5.SolvePart2(GetPuzzleInput());
+            Assert.Equal(24568703, result);
+        }
+
+        private string[] GetPuzzleInput()
+        {
+            return File.ReadAllLines(@"Data\Day5Input.txt");
+        }
+    }
+}
---
+++ 2017/AdventOfCode/Day5.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using AdventOfCode.Supporting;
+using Microsoft.Build.Tasks;
+
+namespace AdventOfCode
+{
+    public class Day5
+    {
+        public static int SolvePart1(string[] input)
+        {
+            var array = input.Select(int.Parse).ToArray();
+            var counter = 0;
+            var index = 0;
+            try
+            {
+                while (true)
+                {
+                    var newIndex = index + array[index];
+                    array[index] = array[index] + 1;
+                    index = newIndex;
+                    counter++;
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+
+            }
+
+            return counter;
+        }
+
+        public static int SolvePart2(string[] input)
+        {
+            var array = input.Select(int.Parse).ToArray();
+            var counter = 0;
+            var index = 0;
+            try
+            {
+                while (true)
+                {
+                    var newIndex = index + array[index];
+                    array[index] = array[index] >= 3 ? array[index] - 1 : array[index] + 1;
+                    index = newIndex;
+                    counter++;
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+
+            }
+
+            return counter;
+        }
+    }
+}
