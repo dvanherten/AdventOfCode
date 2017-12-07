d12315dfc3dfec9d8a6f442a9d4ca103bc9c4c7a
Dave van Herten <dvanherten@outlook.com>
2017-12-07 07:43:27 -0500

    Day 7 Part 1

---
+++ 2017/AdventOfCode.Tests/Day7Tests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using AdventOfCode.Supporting;
+using Xunit;
+
+namespace AdventOfCode.Tests
+{
+    public class Day7Tests
+    {
+        [Fact]
+        public void Part1RealPuzzle()
+        {
+            var result = Day7.SolvePart1(GetPuzzleInput());
+            Assert.Equal("", result);
+        }
+
+        [Fact]
+        public void Part2RealPuzzle()
+        {
+            var result = Day7.SolvePart2(GetPuzzleInput());
+            Assert.Equal(-1, result);
+        }
+
+        [Fact]
+        public void ParseNoChildSuccessfully()
+        {
+            var input = "havc (66)";
+            var program = Day7.Parse(input);
+            Assert.Equal("havc", program.Name);
+            Assert.Equal(66, program.Weight);
+            Assert.Empty(program.ChildProgramNames);
+        }
+
+        [Fact]
+        public void ParseWithChildSuccessfully()
+        {
+            var input = "fwft (72) -> ktlj, cntj, xhth";
+            var program = Day7.Parse(input);
+            Assert.Equal("fwft", program.Name);
+            Assert.Equal(72, program.Weight);
+            Assert.Equal(3, program?.ChildProgramNames.Length);
+        }
+
+        private string[] GetPuzzleInput()
+        {
+            return File.ReadAllLines(@"Data\Day7Input.txt");
+        }
+    }
+}
---
+++ 2017/AdventOfCode/Day7.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using AdventOfCode.Supporting;
+using Microsoft.Build.Tasks;
+
+namespace AdventOfCode
+{
+    public class Day7
+    {
+        public static string SolvePart1(string[] input)
+        {
+            var programs = input.Select(Parse).ToArray();
+            return programs.Single(x => !programs.SelectMany(y => y.ChildProgramNames).Contains(x.Name)).Name;
+        }
+
+        public static int SolvePart2(string[] input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Regex ParseRegex = new Regex(@"^(\w+) \((\d+)\)( -> (.*))?");
+        public static TowerProgram Parse(string input)
+        {
+            var match = ParseRegex.Match(input);
+            return new TowerProgram
+            {
+                Name = match.Groups[1].Value,
+                Weight = int.Parse(match.Groups[2].Value),
+                ChildProgramNames = match.Groups[4].Value.Replace(" ", "").Split(',').Where(x => !string.IsNullOrWhiteSpace(x)).ToArray()
+            }; 
+        }
+
+        public class TowerProgram
+        {
+            public string Name { get; set; }
+            public int Weight { get; set; }
+            public string[] ChildProgramNames { get; set; } = new string[0];
+        }
+    }
+}
