b7c6a0c86ae4ec4e32814b7752bdef473b027741
Dave van Herten <dvanherten@outlook.com>
2017-12-09 07:05:13 -0500

    Day9 - Part 1

---
+++ 2017/AdventOfCode.Tests/Day9Tests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using AdventOfCode.Supporting;
+using Xunit;
+
+namespace AdventOfCode.Tests
+{
+    public class Day9Tests
+    {
+        [Fact]
+        public void Part1RealPuzzle()
+        {
+            var result = Day9.SolvePart1(GetPuzzleInput());
+            Assert.Equal(9251, result);
+        }
+
+        [Fact]
+        public void Part2RealPuzzle()
+        {
+            var result = Day9.SolvePart2(GetPuzzleInput());
+            Assert.Equal(-1, result);
+        }
+
+        private string GetPuzzleInput()
+        {
+            return File.ReadAllLines(@"Data\Day9Input.txt")[0];
+        }
+    }
+}
---
+++ 2017/AdventOfCode/Day9.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    public class Day9
+    {
+        public static int SolvePart1(string input)
+        {
+            var score = WalkStream(input);
+            return score;
+        }
+
+        public static int SolvePart2(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static int WalkStream(string input)
+        {
+            var score = 0;
+            var currentScoreValue = 0;
+            var inGarbage = false;
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                switch (input[i])
+                {
+                    case '!':
+                        i++; // Skip next input.
+                        break;
+                    case '<':
+                        inGarbage = true;
+                        break;
+                    case '>':
+                        inGarbage = false;
+                        break;
+                    case '{':
+                        if (!inGarbage)
+                            currentScoreValue++;
+                        break;
+                    case '}':
+                        if (!inGarbage)
+                        {
+                            score += currentScoreValue;
+                            currentScoreValue--;
+                        }
+                        break;
+                }
+            }
+
+            return score;
+        }
+    }
+}
