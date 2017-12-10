d5803370d7197e81eb8249b3c7242846f0f117f6
Dave van Herten <dvanherten@outlook.com>
2017-12-10 09:32:56 -0500

    Day 10

---
+++ 2017/AdventOfCode.Tests/Day10Tests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using AdventOfCode.Supporting;
+using Xunit;
+
+namespace AdventOfCode.Tests
+{
+    public class Day10Tests
+    {
+        [Fact]
+        public void Part1SampleData()
+        {
+            var result = Day10.SolvePart1("3,4,1,5".Split(','), 5);
+            Assert.Equal(12, result);
+        }
+
+        [Fact]
+        public void Part1RealPuzzle()
+        {
+            var result = Day10.SolvePart1(GetPuzzleInput().Split(','), 256);
+            Assert.Equal(52070, result);
+        }
+
+        [Theory]
+        [InlineData("", "a2582a3a0e66e6e86e3812dcb672a272")]
+        [InlineData("AoC 2017", "33efeb34ea91902bb2f59c9920caa6cd")]
+        [InlineData("1,2,3", "3efbe78a8d82f29979031a4aa0b16a9d")]
+        [InlineData("1,2,4", "63960835bcdc130f0b66d7ff4f6a5a8e")]
+        public void Part2SampleData(string input, string expected)
+        {
+            var result = Day10.SolvePart2(input, 256);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2RealPuzzle()
+        {
+            var result = Day10.SolvePart2(GetPuzzleInput(), 256);
+            Assert.Equal("7f94112db4e32e19cf6502073c66f9bb", result);
+        }
+
+        [Fact]
+        public void Test()
+        {
+            byte index = 255;
+            unchecked
+            {
+                index += 10;
+            };
+            Assert.Equal(9, index);
+
+            int index2 = (255 + 10) % 256;
+            Assert.Equal(9, index2);
+        }
+
+        private string GetPuzzleInput()
+        {
+            return "46,41,212,83,1,255,157,65,139,52,39,254,2,86,0,204";
+        }
+    }
+}
---
+++ 2017/AdventOfCode/Day10.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using AdventOfCode.Supporting;
+using Microsoft.Build.Tasks;
+
+namespace AdventOfCode
+{
+    public class Day10
+    {
+        public static int SolvePart1(string[] input, int arraySize)
+        {
+            var lengths = input.Select(int.Parse).ToArray();
+            var sparseHash = GetSparseHash(lengths, arraySize);
+            return sparseHash[0] * sparseHash[1];
+        }
+        
+        public static string SolvePart2(string input, int arraySize)
+        {
+            var lengths = GenerateLengths(input);
+            var sparseHash = GetSparseHash(lengths, arraySize, 64);
+            var denseHash = BuildDenseHash(sparseHash);
+            var knotHash = ConvertToKnotHash(denseHash);
+            return knotHash;
+        }
+
+        private static int[] GetSparseHash(int[] lengths, int arraySize, int repeat = 1)
+        {
+            var sparseHash = BuildInitialArray(arraySize);
+            var currentPosition = 0;
+            var skipSize = 0;
+
+            for (var loop = 0; loop < repeat; loop++)
+            {
+                foreach (var length in lengths)
+                {
+                    // Lengths larger than the size of the list are invalid.
+                    if (length > sparseHash.Length)
+                        continue;
+
+                    var subArray = GetSubArray(sparseHash, length, currentPosition);
+                    subArray = subArray.Reverse().ToArray();
+                    for (var i = 0; i < length; i++)
+                    {
+                        var offSetIndex = (currentPosition + i) % sparseHash.Length;
+                        sparseHash[offSetIndex] = subArray[i];
+                    }
+                    currentPosition = (currentPosition + length + skipSize) % sparseHash.Length;
+                    skipSize++;
+                }
+            }
+
+            return sparseHash;
+        }
+
+        private static int[] BuildDenseHash(int[] sparseHash)
+        {
+            var denseHash = new int[16];
+
+            for (var i = 0; i < 16; i++)
+                denseHash[i] = sparseHash
+                    .Skip(i * 16)
+                    .Take(16)
+                    .Aggregate((running, next) => running ^ next);
+            
+            return denseHash;
+        }
+
+        private static string ConvertToKnotHash(int[] denseHash)
+        {
+            return string.Join("", denseHash.Select(x => x.ToString("X2"))).ToLower();
+        }
+
+        public static int[] GenerateLengths(string input)
+        {
+            var lengths = input.Select(x => (int)x).ToArray();
+            return lengths.Concat(new int[] {17, 31, 73, 47, 23}).ToArray();
+        }
+
+        private static int[] BuildInitialArray(int arraySize)
+        {
+            var array = new int[arraySize];
+            for (int i = 0; i < array.Length; i++)
+                array[i] = i;
+            return array;
+        }
+
+        private static int[] GetSubArray(int[] sourceArray, int length, int startingIndex)
+        {
+            var array = sourceArray.Skip(startingIndex).Take(length).ToArray();
+            var remainder = length - array.Length;
+            if (remainder > 0)
+                array = array.Concat(sourceArray.Take(remainder)).ToArray();
+            return array;
+        }
+    }
+}
