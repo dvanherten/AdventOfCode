3423dc7a179753086ed8f18e5bb2bcad4fb395bf
Dave van Herten <dvanherten@outlook.com>
2017-12-13 18:02:14 -0500

    Day 13 - Part 1

---
+++ 2017/AdventOfCode.Tests/Day13Tests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using AdventOfCode.Supporting;
+using Xunit;
+
+namespace AdventOfCode.Tests
+{
+    public class Day13Tests
+    {
+        [Fact]
+        public void Part1Sample()
+        {
+            var puzzleInput = new[] {"0: 3", "1: 2", "4: 4", "6: 4"};
+            var result = Day13.SolvePart1(puzzleInput);
+            Assert.Equal(24, result);
+        }
+
+        [Fact]
+        public void Part1RealPuzzle()
+        {
+            var result = Day13.SolvePart1(GetPuzzleInput());
+            Assert.Equal(2160, result);
+        }
+
+        [Fact]
+        public void Part2RealPuzzle()
+        {
+            var result = Day13.SolvePart2(GetPuzzleInput());
+            Assert.Equal(-1, result);
+        }
+
+        private string[] GetPuzzleInput()
+        {
+            return File.ReadAllLines(@"Data\Day13Input.txt");
+        }
+    }
+}
---
+++ 2017/AdventOfCode/Day13.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode
+{
+    public class Day13
+    {
+        public static int SolvePart1(string[] getPuzzleInput)
+        {
+            var layerScanners = getPuzzleInput.Select(x =>
+            {
+                var split = x.Split(new []{": "}, StringSplitOptions.None).Select(int.Parse).ToArray();
+                return new LayerScanner { Depth = split[0], Range = split[1]};
+            }).ToArray();
+
+            var maxDepth = layerScanners.Max(x => x.Depth);
+            var totalSeverity = 0;
+            for (int picosecond = 0; picosecond <= maxDepth; picosecond++)
+            {
+                foreach (var layerScanner in layerScanners)
+                {
+                    if (layerScanner.Depth == picosecond && layerScanner.ScannerLocation == 1)
+                        totalSeverity += layerScanner.Severity;
+                    layerScanner.MoveScanner();
+                }
+            }
+            return totalSeverity;
+        }
+
+        public static int SolvePart2(string[] getPuzzleInput)
+        {
+            throw new NotImplementedException();
+        }
+
+        public class LayerScanner
+        {
+            public int Depth { get; set; }
+            public int Range { get; set; }
+            public int ScannerLocation { get; private set; } = 1;
+            public bool MovingDown { get; private set; } = true;
+            public int Severity => Depth * Range;
+            
+            public void MoveScanner()
+            {
+                ScannerLocation = MovingDown ? ScannerLocation + 1 : ScannerLocation - 1;
+                if (ScannerLocation == 1 || ScannerLocation == Range)
+                    MovingDown = !MovingDown;
+            }
+        }
+    }
+}
