3ed3e703cc2192c04532e21720beff636594270b
Dave van Herten <dvanherten@outlook.com>
2017-12-14 07:17:19 -0500

    Day 14

---
+++ 2017/AdventOfCode.Tests/Day14Tests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using AdventOfCode.Supporting;
+using Xunit;
+
+namespace AdventOfCode.Tests
+{
+    public class Day14Tests
+    {
+        public void Part1SamplePuzzle()
+        {
+            var result = Day14.SolvePart1("flqrgnkx");
+            Assert.Equal(8108, result);
+        }
+
+        [Fact]
+        public void Part1RealPuzzle()
+        {
+            var result = Day14.SolvePart1(GetPuzzleInput());
+            Assert.Equal(8250, result);
+        }
+
+        [Fact(Skip = "Takes too long. :(")]
+        public void Part2SamplePuzzle()
+        {
+            var result = Day14.SolvePart2("flqrgnkx");
+            Assert.Equal(1242, result);
+        }
+
+        [Fact(Skip = "Takes too long. :(")]
+        public void Part2RealPuzzle()
+        {
+            var result = Day14.SolvePart2(GetPuzzleInput());
+            Assert.Equal(-1, result);
+        }
+
+        private string GetPuzzleInput()
+        {
+            return "stpzcrnm";
+        }
+    }
+}
---
+++ 2017/AdventOfCode/Day14.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using AdventOfCode.Supporting;
+
+namespace AdventOfCode
+{
+    public class Day14
+    {
+        public static int SolvePart1(string input)
+        {
+            var squaresUsed = 0;
+            for (var i = 0; i < 128; i++)
+            {
+                var knotHash = Day10.SolvePart2($"{input}-{i}", 256);
+                var binaryString = String.Join("", knotHash.Select(x => Convert.ToString(Convert.ToInt32(x.ToString(), 16), 2).PadLeft(4, '0')));
+                squaresUsed += binaryString.Replace("0", "").Length;
+            }
+            return squaresUsed;
+        }
+
+        public static int SolvePart2(string input)
+        {
+            var squares = new List<DiskSquare>();
+            for (var i = 0; i < 128; i++)
+            {
+                var knotHash = Day10.SolvePart2($"{input}-{i}", 256);
+                var binaryString = String.Join("", knotHash.Select(x => Convert.ToString(Convert.ToInt32(x.ToString(), 16), 2).PadLeft(4, '0')));
+                for (int j = 0; j < binaryString.Length; j++)
+                    squares.Add(new DiskSquare {Point = new Point(j, i), Used = binaryString[j] == '1'});
+            }
+
+            var currentRegionNumber = 0;
+            foreach (var square in squares)
+            {
+                if (square.Used && square.RegionNumber == null)
+                {
+                    var queue = new Queue<DiskSquare>(new [] {square});
+                    while (queue.Count != 0)
+                    {
+                        var queueSquare = queue.Dequeue();
+                        var neighbours = squares.Where(x =>
+                            x.Used && Day3.CalculateManhattanDistance(queueSquare.Point, x.Point) == 1).ToArray();
+
+                        var neighbourRegionNumber = neighbours.Where(x => x.RegionNumber.HasValue)
+                            .Select(x => x.RegionNumber).FirstOrDefault();
+
+                        queueSquare.RegionNumber = neighbourRegionNumber ?? ++currentRegionNumber;
+                        foreach (var neighbour in neighbours.Where(x => !x.RegionNumber.HasValue))
+                            queue.Enqueue(neighbour);
+                    }
+                }
+            }
+            return currentRegionNumber;
+        }
+
+
+        [DebuggerDisplay("X = {Point.X}, Y = {Point.Y}, Used = {Used}, RegionNumber = {RegionNumber}")]
+        public class DiskSquare
+        {
+            public Point Point { get; set; }
+            public bool Used { get; set; }
+            public int? RegionNumber { get; set; }
+        }
+    }
+}
