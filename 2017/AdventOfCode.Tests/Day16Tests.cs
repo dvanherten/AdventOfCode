35aad8efaafe68de463ff9dca5f2ef692a7c6aa4
Dave van Herten <dvanherten@outlook.com>
2017-12-16 10:33:56 -0500

    Day 16 - % for the win
    Brute force wouldn't work here so. Perused reddit and someone explained why mod helps (assuming you get a repeat). Implemented and weeeeee

---
+++ 2017/AdventOfCode.Tests/Day16Tests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using AdventOfCode.Supporting;
+using Xunit;
+
+namespace AdventOfCode.Tests
+{
+    public class Day16Tests
+    {
+        [Fact]
+        public void Part1Sample()
+        {
+            var dancers = new Day16.ArrayThatDancesWoooo("abcde");
+            dancers.Dance("s1");
+            Assert.Equal("eabcd", dancers.ToString());
+            dancers.Dance("x3/4");
+            Assert.Equal("eabdc", dancers.ToString());
+            dancers.Dance("pe/b");
+            Assert.Equal("baedc", dancers.ToString());
+        }
+
+        [Fact]
+        public void Part1RealPuzzle()
+        {
+            var input = GetPuzzleInput();
+            var result = Day16.SolvePart1(input);
+            Assert.Equal("namdgkbhifpceloj", result);
+        }
+
+        [Fact]
+        public void Part2Sample()
+        {
+            var input = "s1,x3/4,pe/b";
+            var result = Day16.SolvePart2(input, 2, "abcde");
+            Assert.Equal("ceadb", result);
+        }
+
+        [Fact]
+        public void Part2RealPuzzle()
+        {
+            var input = GetPuzzleInput();
+            var result = Day16.SolvePart2(input, 1000000000);
+            Assert.Equal("ibmchklnofjpdeag", result);
+        }
+
+        private string GetPuzzleInput()
+        {
+            return File.ReadAllLines(@"Data\Day16Input.txt")[0];
+        }
+    }
+}
---
+++ 2017/AdventOfCode/Day16.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using AdventOfCode.Supporting;
+using Microsoft.Build.Tasks;
+
+namespace AdventOfCode
+{
+    public class Day16
+    {
+
+        public static string SolvePart1(string danceMoves, string start = "abcdefghijklmnop")
+        {
+            var dancers = new ArrayThatDancesWoooo(start);
+            foreach (var move in danceMoves.Split(','))
+                dancers.Dance(move);
+            return dancers.ToString();
+        }
+
+        public static string SolvePart2(string danceMoves, int cycles, string start = "abcdefghijklmnop")
+        {
+            var dancers = new ArrayThatDancesWoooo(start);
+            var counter = 0;
+            do
+            {
+                foreach (var move in danceMoves.Split(','))
+                    dancers.Dance(move);
+
+                counter++;
+            } while (dancers.ToString() != start && counter < cycles);
+
+            // Completed cycles? We're done.
+            if (counter == cycles)
+                return dancers.ToString();
+
+            // Back at the beginning? The answer is in the remainder.
+            var cyclesLeft = cycles % counter;
+            for (var i = 0; i < cyclesLeft; i++)
+                foreach (var move in danceMoves.Split(','))
+                    dancers.Dance(move);
+
+            return dancers.ToString();
+        }
+
+        public class ArrayThatDancesWoooo
+        {
+            private List<char> _dancers;
+            private readonly Regex SwapRegex = new Regex(@"[a-z](.+)\/(.+)");
+
+            public ArrayThatDancesWoooo(string input)
+            {
+                _dancers = input.Select(x => x).ToList();
+            }
+
+            public void Dance(string move)
+            {
+                if (move[0] == 's')
+                {
+                    Spin(int.Parse(string.Join("", move.Skip(1))));
+                }
+                else if (move[0] == 'x')
+                {
+                    var match = SwapRegex.Match(move);
+                    Exchange(int.Parse(match.Groups[1].ToString()), int.Parse(match.Groups[2].ToString()));
+                }
+                else if (move[0] == 'p')
+                {
+                    var match = SwapRegex.Match(move);
+                    Partner(char.Parse(match.Groups[1].ToString()), char.Parse(match.Groups[2].ToString()));
+                }
+            }
+
+            private void Spin(int size)
+            {
+                var splitAmount = _dancers.Count - size;
+                var firstHalf = _dancers.Take(splitAmount);
+                var secondHalf = _dancers.Skip(splitAmount);
+                _dancers = secondHalf.Concat(firstHalf).ToList();
+            }
+
+            private void Exchange(int index1, int index2)
+            {
+                var temp = _dancers[index1];
+                _dancers[index1] = _dancers[index2];
+                _dancers[index2] = temp;
+            }
+
+            private void Partner(char value1, char value2)
+            {
+                var index1 = _dancers.IndexOf(value1);
+                var index2 = _dancers.IndexOf(value2);
+                Exchange(index1, index2);
+            }
+
+            public override string ToString()
+            {
+                return new string(_dancers.ToArray());
+            }
+        }
+    }
+}
