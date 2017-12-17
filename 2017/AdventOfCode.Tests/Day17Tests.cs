ef3da0d5cea2aa4243b31ffe5f15555f8cc8f203
Dave van Herten <dvanherten@outlook.com>
2017-12-17 08:44:37 -0500

    Day 17 - Part 1

---
+++ 2017/AdventOfCode.Tests/Day17Tests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using AdventOfCode.Supporting;
+using Xunit;
+
+namespace AdventOfCode.Tests
+{
+    public class Day17Tests
+    {
+        [Fact]
+        public void Part1Sample()
+        {
+            var result = Day17.SolvePart1(3);
+            Assert.Equal(638, result);
+        }
+
+        [Fact]
+        public void Part1RealPuzzle()
+        {
+            var input = GetPuzzleInput();
+            var result = Day17.SolvePart1(input);
+            Assert.Equal(1487, result);
+        }
+        
+        [Fact]
+        public void Part2RealPuzzle()
+        {
+            var input = GetPuzzleInput();
+            var result = Day17.SolvePart2(input);
+            Assert.Equal(-1, result);
+        }
+
+        private int GetPuzzleInput()
+        {
+            return 367;
+        }
+    }
+}
---
+++ 2017/AdventOfCode/Day17.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using AdventOfCode.Supporting;
+using Microsoft.Build.Tasks;
+
+namespace AdventOfCode
+{
+    public class Day17
+    {
+
+        public static int SolvePart1(int input)
+        {
+            var list = BuildList(input, 2017);
+            var index = list.IndexOf(2017);
+            return list[index + 1];
+        }
+
+        public static int SolvePart2(int input)
+        {
+            var list = BuildList(input, 50000000);
+            var index = list.IndexOf(0);
+            return list[index + 1];
+        }
+
+        public static List<int> BuildList(int input, int loops)
+        {
+            var list = new List<int> {0};
+            var currentIndex = 0;
+
+            for (var i = 1; i <= loops; i++)
+            {
+                // Move
+                var spacesToMove = input % i;
+                var newIndex = currentIndex + spacesToMove;
+                currentIndex = newIndex >= list.Count
+                    ? newIndex - list.Count
+                    : newIndex;
+
+                // Insert
+                currentIndex++;
+                list.Insert(currentIndex, i);
+            }
+
+            return list;
+        }
+    }
+}
