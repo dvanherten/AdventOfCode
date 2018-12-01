620d92a81ef292f8cc85704d16b37882a6e7ce51
Dave van Herten <dvanherten@outlook.com>
2018-12-01 00:26:53 -0500

    Advent 2018 - Day 1

--- 2018/days/day01.js
+++ 2018/days/day01.js
@@ -1,17 +1,41 @@
-'use strict'
+"use strict";
 
 // Part 1
 // ======
 
 const part1 = input => {
-  return input
-}
+  const frequencyChanges = input.split("\n").map(x => +x);
+  const trackFrequency = (frequency, change) => frequency + change;
+  const result = frequencyChanges.reduce(trackFrequency);
+  return result;
+};
 
 // Part 2
 // ======
 
 const part2 = input => {
-  return input
-}
+  const frequencyChanges = input.split("\n").map(x => +x);
+  const frequenciesSeen = [];
 
-module.exports = { part1, part2 }
+  let valuesFoundTwice = null;
+  const trackFrequency = (accumulator, currentValue) => {
+    const value = accumulator + currentValue;
+    if (frequenciesSeen.includes(value) && valuesFoundTwice == null)
+      valuesFoundTwice = value;
+    else frequenciesSeen.push(value);
+    return value;
+  };
+
+  let currentFrequency = 0;
+  let endlessPrevention = 0;
+  while (valuesFoundTwice == null || endlessPrevention === 100) {
+    currentFrequency = frequencyChanges.reduce(
+      trackFrequency,
+      currentFrequency
+    );
+    endlessPrevention++;
+  }
+  return valuesFoundTwice;
+};
+
+module.exports = { part1, part2 };
