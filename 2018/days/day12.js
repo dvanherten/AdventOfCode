7789bcd161875b8588e8cdd7e7b49238e56649cd
Dave van Herten <dvanherten@outlook.com>
2018-12-12 21:04:40 -0500

    day 12 part 2

--- 2018/days/day12.js
+++ 2018/days/day12.js
@@ -22,16 +22,14 @@ const testInput = `initial state: #..#.#..##......###...###
 
 const part1 = input => {
   const parsedInput = parse(input);
-  const rules = parsedInput.rules
-    .map(r => {
-      const [ruleString, outputString] = r.split(" => ");
-      const rule = ruleString.split("").map(toBool);
-      return {
-        rule: rule,
-        output: toBool(outputString)
-      };
-    })
-    .filter(r => r.output);
+  const rules = parsedInput.rules.map(r => {
+    const [ruleString, outputString] = r.split(" => ");
+    const rule = ruleString.split("").map(toBool);
+    return {
+      rule: rule,
+      output: toBool(outputString)
+    };
+  });
   const negativeSpace = 10;
   const gen0 = [...new Array(negativeSpace).fill(false)]
     .concat(parsedInput.initial.split("").map(toBool))
@@ -67,7 +65,58 @@ const part1 = input => {
 // ======
 
 const part2 = input => {
-  return input;
+  const parsedInput = parse(input);
+  const rules = parsedInput.rules.map(r => {
+    const [ruleString, outputString] = r.split(" => ");
+    const rule = ruleString.split("").map(toBool);
+    return {
+      rule: rule,
+      output: toBool(outputString)
+    };
+  });
+
+  const negativeSpace = 5;
+  const gen0 = [...new Array(negativeSpace).fill(false)]
+    .concat(parsedInput.initial.split("").map(toBool))
+    .concat([...new Array(1000).fill(false)]); // Allow for lots of growth on the positive side.
+
+  const generations = [];
+  generations.push(gen0);
+  const length = gen0.length;
+
+  let prev = gen0;
+  let lastSum = 0;
+  let lastDiff = 0;
+  let sameDiffCounter = 0;
+
+  let counter = 0;
+  // Look for linear progression
+  for (counter; counter < 1000; counter++) {
+    const next = new Array(length).fill(false);
+    for (let r = 0; r < rules.length; r++) {
+      for (let section = 5; section < length; section++) {
+        const slice = prev.slice(section - 5, section);
+        if (slice.every((x, i) => x === rules[r].rule[i]))
+          next[section - 3] = rules[r].output;
+      }
+    }
+
+    const newSum = next.reduce(
+      (total, val, index) => total + (val ? index - negativeSpace : 0),
+      0
+    );
+    const newDiff = newSum - lastSum;
+    if (newDiff == lastDiff) sameDiffCounter++;
+    else sameDiffCounter = 0;
+    if (sameDiffCounter == 5) break; // Assumption that if we see the same diff 5 times in a row we will repeat forever.
+
+    lastSum = newSum;
+    lastDiff = newDiff;
+    generations.push(next);
+    prev = next;
+  }
+  const wayTooManyIterations = 50000000000;
+  return lastSum + (wayTooManyIterations - counter) * lastDiff;
 };
 
 module.exports = { part1, part2 };
